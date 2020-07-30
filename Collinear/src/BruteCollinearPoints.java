90b4a107303feda7bd061225d149f0d73b47235d
Ethan Kim <robhn38426@gmail.com>
2020-07-29 20:41:53 -0700

    Fix Test 11: mutate constructor argument

--- Collinear/src/BruteCollinearPoints.java
+++ Collinear/src/BruteCollinearPoints.java
@@ -15,25 +15,28 @@ public class BruteCollinearPoints {
     public BruteCollinearPoints(Point[] points) {
 
 	validateInput(points);
+	
+	// copy points array to avoid mutating original's values
+	Point[] pointsCopy = Arrays.copyOf(points, points.length);
 
 	// sort array by natural order: from low to high y coordinate
 	// if tied, sort by x coordinate.
 	// this way, lines segments are formed in ascending order.
 	// Endpoints are clear during loop.
-	Arrays.sort(points);
+	Arrays.sort(pointsCopy);
 	
 	// an ArrayList to store linesegments. Dynamic capacity & can find size
 	ArrayList<LineSegment> connects4List = new ArrayList<LineSegment>();
 
-	for (int p = 0; p < points.length - 3; p++) {
-	    for (int q = p + 1; q < points.length - 2; q++) {
-		for (int r = q + 1; r < points.length - 1; r++) {
-		    for (int s = r + 1; s < points.length; s++) {
-			double slopepq = points[p].slopeTo(points[q]);
-			double slopeqr = points[q].slopeTo(points[r]);
-			double slopers = points[r].slopeTo(points[s]);
+	for (int p = 0; p < pointsCopy.length - 3; p++) {
+	    for (int q = p + 1; q < pointsCopy.length - 2; q++) {
+		for (int r = q + 1; r < pointsCopy.length - 1; r++) {
+		    for (int s = r + 1; s < pointsCopy.length; s++) {
+			double slopepq = pointsCopy[p].slopeTo(pointsCopy[q]);
+			double slopeqr = pointsCopy[q].slopeTo(pointsCopy[r]);
+			double slopers = pointsCopy[r].slopeTo(pointsCopy[s]);
 			if (slopepq == slopeqr && slopeqr == slopers) {
-			    connects4List.add(new LineSegment(points[p], points[s]));
+			    connects4List.add(new LineSegment(pointsCopy[p], pointsCopy[s]));
 			}
 		    }
 		}
